baabcc1f4b3b4fa52ecb7d1cbef8c40436d993e5
Seyedehzahrakhalili <166828528+Seyedehzahrakhalili@users.noreply.github.com>
2024-05-11 15:35:16 +0330

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,21 @@
+/* برنامه ای بنویسید که با پیام مناسب یک عدد بین ۰ الی ۱۰ بگیرید سپس چک کنید که آیا زوج است یا خیر ، اگر زوج بود مشخص کنید که عدد کمتر از ۵ است یا بیشتر از ۵ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<" Enter a number between 1 and 10"<<endl;
+    cin>>a;
+    if(a>=0&&a<11){   
+    if(a%2==1){
+    cout<<"Odd";
+    }
+    else if(a%2==0&&a>5){
+    cout<<"even"<<"&"<<a<<">5"<<endl;
+    }
+    else{
+    cout<<"even"<<"&"<<a<<"<5"<<endl;
+    }
+}
+    return 0;
+}
