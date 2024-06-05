9db3711fa7e6404de66cb81f14953ef5b79c26e1
Seyedehzahrakhalili <166828528+Seyedehzahrakhalili@users.noreply.github.com>
2024-06-05 13:48:37 +0330

    Add files via upload

---
+++ main (50).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char c;
+    cout<<"enter a number";
+    cin>>a;
+    cout<<"enter a character";
+    cin>>c;
+    for(int i=0;i<a;i++){
+        cout<<c;
+    }
+
+    return 0;
+}
