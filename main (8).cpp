3683083adce5434ffe29b021721634490d2b2a0a
Seyedehzahrakhalili <166828528+Seyedehzahrakhalili@users.noreply.github.com>
2024-06-05 11:23:40 +0330

    Update main (8).cpp

--- main (8).cpp
+++ main (8).cpp
@@ -8,9 +8,12 @@ int main()
         cin>>x1>>y1;
         cout<<"Please enter x2 & y2"<<endl;
         cin>>x2>>y2;
+        if(x2-x1!=0){
         a=(y2-y1)/(x2-x1);
-        x2-x1!=0;
         b=y1-(a*x1);
         cout<<"y"<<"="<<a<<"*x+"<<b;
+        }else{
+                cout<<"error";
+        }       
     return 0;
 }
